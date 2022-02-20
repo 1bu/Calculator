33551e690744fcaeafe71fdd6809a3f42977a0f9
Iván Carpio <67491616+1bu@users.noreply.github.com>
2022-02-20 01:02:01 -0500

    v2.0

--- routes/calculator.js
+++ routes/calculator.js
@@ -1,3 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+///////////////////////////////Calculo////////////////////////////////
+///SUMA
+function add (num1,num2){
+    let n1 = parseInt(num1,10);
+    let n2 = parseInt(num2,10);
+
+    if(n1 && n2){
+        return(n1 + n2);
+    }
+}
+///RESTA
+function subtraction(num1,num2){
+    let n1 = parseInt(num1,10);
+    let n2 = parseInt(num2,10);
+
+    if(n1 && n2){
+        return (n1 - n2);
+    }
+}
+
+//Multipliacion
+function multiplication(num1,num2){
+    let n1 = parseInt(num1,10);
+    let n2 = parseInt(num2,10);
+
+    if(n1 && n2){
+        return(n1 * n2);
+    }
+}
+function division(num1,num2){
+    let n1 = parseInt(num1,10);
+    let n2 = parseInt(num2,10);
+
+    if(n1 && n2){
+        return(n1 / n2);
+    }
+}
+
+module.exports = {add, subtraction, multiplication, division};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-//const calculator = require('../routes/calculator');
+const calculator = require('../routes/calculator');
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false}));
@@ -12,28 +12,30 @@ app.get('/:op/:n1/:n2', (req,res) =>{
 
     console.log(req.params.op);
     let resul;
+    const n1 = req.params.n1;
+    const n2 = req.params.n2;
     const data = {
         "Result":resul,
         "Description":req.params.n1 + "+" + req.params.n2 + "= "
     };
 
     switch(req.params.op){
-        case 'add': resul = add(req.params.n1,req.params.n2);
+        case 'add': resul = calculator.add(n1,n2); 
         data.Result = resul; data.Description = req.params.n1 + "+" + req.params.n2 + "=" + resul;
         res.json(data);
         break;  
 
-        case 'sub': resul = subtraction(req.params.n1,req.params.n2);
+        case 'sub': resul = calculator.subtraction(n1,n2);
         data.Result = resul; data.Description = req.params.n1 + "-" + req.params.n2 + "=" + resul;
         res.json(data);
         break;
 
-        case 'mult': resul = multiplication(req.params.n1,req.params.n2); 
+        case 'mult': resul = calculator.multiplication(n1,n2);
         data.Result = resul; data.Description = req.params.n1 + "*" + req.params.n2 + "=" + resul;
         res.json(data);
         break;
 
-        case 'div': resul = division(req.params.n1,req.params.n2); 
+        case 'div': resul = calculator.division(n1,n2);
         data.Result = resul; data.Description = req.params.n1 + "/" + req.params.n2 + "=" + resul;
         res.json(data);
         break;
@@ -44,57 +46,6 @@ app.get('/:op/:n1/:n2', (req,res) =>{
 
 }); 
 
-
 app.listen(port, ()=>{
     console.log(`Server on port ${port}`);
 });
-
-///////////////////////////////Calculo////////////////////////////////
-///SUMA
-function add (num1,num2){
-    let n1 = parseInt(num1,10);
-    let n2 = parseInt(num2,10);
-
-    if(n1 && n2){
-        return(n1 + n2);
-        /*res.send(add+"");
-        console.log(res);
-        return(res);*/
-    }
-}
-///RESTA
-function subtraction(num1,num2){
-    let n1 = parseInt(num1,10);
-    let n2 = parseInt(num2,10);
-
-    if(n1 && n2){
-        return (n1 - n2);
-        /*res.send(resul+"");
-        console.log(resul);
-        return(resul);*/
-    }
-}
-
-//Multipliacion
-function multiplication(num1,num2){
-    let n1 = parseInt(num1,10);
-    let n2 = parseInt(num2,10);
-
-    if(n1 && n2){
-        return(n1 * n2);
-        /*//res.send(resul+"");
-        console.log(resul);
-        return(resul);*/
-    }
-}
-function division(num1,num2){
-    let n1 = parseInt(num1,10);
-    let n2 = parseInt(num2,10);
-
-    if(n1 && n2){
-        return(n1 / n2);
-        /*//res.send(resul+"");
-        console.log(resul);
-        return(resul);*/
-    }
-}
